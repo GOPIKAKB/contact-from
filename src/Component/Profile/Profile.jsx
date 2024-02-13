7a462cf2fcc7ed3779d9e8559ccd71b3e78f903a
GOPIKAKB <gopikakrishnakb@gmail.com>
2024-02-13 15:55:39 +0530

    Update Profile.jsx

--- src/Component/Profile/Profile.jsx
+++ src/Component/Profile/Profile.jsx
@@ -23,10 +23,14 @@ function Profile() {
             <h2 >Profile Page</h2>
             <div class="user-details">
                 <img src="https://www.clipartkey.com/mpngs/m/146-1461473_default-profile-picture-transparent.png" alt="User" class="user-image" />
-                <p class="user-info">Name: {user?.displayName} || Gopika</p>
-                <p class="user-info">Email: {user?.email} || abc</p>
-                <p class="user-info">Phone Number: {user?.phoneNumber || 'N/A'}</p>
-                <button onClick={logout} class="logout-button">Logout</button>
+                <div className='user-info-container'>
+                    <h2 class="user-info">{user?.displayName} || Gopika</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</p>
+                    <p class="user-info">Email: {user?.email} || abc</p>
+                    <p class="user-info">Phone Number: {user?.phoneNumber || 'N/A'}</p>
+                    <button onClick={logout} class="logout-button">Logout</button>
+
+                </div>
 
             </div>
         </div>
